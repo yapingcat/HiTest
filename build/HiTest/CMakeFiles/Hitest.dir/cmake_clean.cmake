ea22011dedad28c632f1529fb77b8ef34e4277ee
root <caoyaping@163.com>
2019-12-12 23:45:04 +0800

    change Test.cpp encode,fix cmake build error

---
+++ build/HiTest/CMakeFiles/Hitest.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Hitest.dir/src/main.cpp.o"
+  "libHitest.pdb"
+  "libHitest.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Hitest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/HiTest/CMakeFiles/Hitest.dir/cmake_clean_target.cmake
@@ -0,0 +1,3 @@
+file(REMOVE_RECURSE
+  "libHitest.a"
+)
